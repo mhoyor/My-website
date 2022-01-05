56a2e6305fb15e29eb7c0a2ac042e64a4bd705c9
unknown <aiyewumiv@gmail.com>
2022-01-06 00:10:38 +0100

    Update

--- swal.js
+++ swal.js
@@ -3,7 +3,7 @@ $(document).on('click', '#btn_submit', function (e) {
   Swal.fire({
     title: 'Thanks for reaching out',
     icon: 'success',
-    footer: 'I will get back soon'
+    footer: 'I will get back soon',
     showClass: {
       popup: 'animate__animated animate__fadeInDown',
     },
